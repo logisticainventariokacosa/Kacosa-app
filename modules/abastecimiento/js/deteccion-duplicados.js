118eee55471ab0ec55bcd9c195f3b388cc4beb0b
logisticainventariokacosa <logisticainventariokacosa@gmail.com>
2026-09-13 18:09:43 -0400

    Update import version for obtenerFactor function

--- modules/abastecimiento/js/deteccion-duplicados.js
+++ modules/abastecimiento/js/deteccion-duplicados.js
@@ -1,5 +1,5 @@
 // js/deteccion-duplicados.js
-import { obtenerFactor } from "./factores-conversion.js?v=1";
+import { obtenerFactor } from "./factores-conversion.js?v=2";
 
 /** Normaliza texto para comparar: mayúsculas, sin acentos, sin espacios repetidos. */
 function normalizar(texto) {
